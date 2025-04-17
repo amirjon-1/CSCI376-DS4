8abacbb535656964d99349f954aec6a68bc835e7
Adhip Rijal <adhiprijal@Adhips-MacBook-Pro-1027.local>
2025-04-16 22:46:33 -0400

    a push

--- script.js
+++ script.js
@@ -4,21 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const highlightBtn = document.querySelector("#highlightButton");
     const resetBtn = document.querySelector("#resetButton");
     const clearBtn = document.querySelector("#clearButton");
+    const fivestarbooks = [
+      "To Kill a Mockingbird",
+      "Moby-Dick",
+      "The Catcher in the Rye"
+    ];
   
     // ----- EVENTS -----
     highlightBtn.addEventListener("click", () => {
       books.forEach(book => {
         const title = book.querySelector("h2").textContent.trim();
-        if (title.length > 20) {
+        if (fivestarbooks.includes(title)) {
           book.classList.remove("border-primary");
-          book.classList.add("border-danger", "border-3");
+          book.classList.add("border-warning", "border-5");
         }
       });
     });
   
     resetBtn.addEventListener("click", () => {
       books.forEach(book => {
-        book.classList.remove("border-danger", "border-3", "bg-info", "text-white", "d-none");
+        book.classList.remove("border-warning", "border-5", "bg-info", "text-white", "d-none");
         book.classList.add("border-secondary");
       });
       searchInput.value = "";
